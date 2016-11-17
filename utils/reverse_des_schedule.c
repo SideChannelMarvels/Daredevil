03a886a3ec8463d2a485fde15aa1aa162f8e8c83
Charles Hubain <chubain@quarkslab.com>
2016-11-17 13:47:13 +0100

    Working on breaking DES key from round key - WIP

---
+++ utils/reverse_des_schedule.c
@@ -0,0 +1,29 @@
+#include "DES.h"
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+    uint64_t key, round_key, next_key, possible_keys[256];
+    if(argc < 2) {
+        printf("Please give a key as argument\n");
+        return 1;
+    }
+    key = strtoull(argv[1], NULL, 16);
+    printf("Input key: %016" PRIx64 "\n", key);
+    round_key = key;
+    key_schedule(&round_key, &next_key, 0);
+    printf("Round key: %016" PRIx64 "\n", round_key);
+    printf("Reversing round key ...\n");
+    reverse_key_schedule(round_key, 0, possible_keys);
+    for(int i = 0; i < 256; i++) {
+        printf("%016" PRIx64 "\n", possible_keys[i]);
+        if(possible_keys[i] == key) {
+            printf("Key found at offset %d!\n", i);
+            return 0;
+        }
+    }
+    printf("Key not found :(\n");
+    return 1;
+}
